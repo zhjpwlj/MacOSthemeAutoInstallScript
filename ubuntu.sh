049be114976c11efe9684055c606896db48f009c
zhjpwlj <141892783+zhjpwlj@users.noreply.github.com>
2026-07-10 20:46:05 +0900

    Update GNOME extensions and theme installation script

--- ubuntu.sh
+++ ubuntu.sh
@@ -155,28 +155,25 @@ configure_firefox() {
 
 install_gnome_extensions{
     extensions=(
-        5489
         1488
         19
         3740
-        1460
         3193
-        1319
         3843
-        5848
+        4105
         779
         5090
-        4422
-        3986
-        3888
-        22
-        4451
-        3951
-        3990
+        4158
+        3733
+        6433
+        4114
+        97
+        2524
+        4994
         4245
-        5387
+        8267
       )
-      # Search light ,Fuzzy search ,User themes, Compiz alike magic lamp effect, Vitals, Blur my shell, Gsconnect, Just perfection, Notification banner position, Clipboard indicator, Space bar, Ubuntu tilting assistant, App menu is back, Fildem global menu, Coverflow alt+tab, Logo menu, Rounded corners, Dash2dock animated, Gesture improvements, Control center
+        #Fuzzy search, User themes, Compiz alike magic lamp effect, Blur my shell, Just perfection, Notification banner position, Clipboard indicator, Space bar, gnome 4x 5x improvement, Ubuntu tilting assistant, App menu is back, Fildem global menu, Coverflow alt+tab, Rounded corners, Dash2dock animated, Gesture improvements, Kiwi is not apple
   
       for ext in "${extensions[@]}"; do
         echo "Installing extension $ext..."
@@ -194,6 +191,5 @@ install_theme(){
     git clone https://github.com/vinceliuice/MacTahoe-gtk-theme.git --depth=1
     cd MacTahoe-gtk-theme
     sudo ./install.sh -o normal -b --shell -i apple -h smaller -sf --round --silent-mode
-    ./install.sh -l -o normal -b --shell -i apple -h smaller -sf --round --silent-mode
-    
+    ./install.sh -l -o normal -b --shell -i apple -h smaller -sf --round
     
